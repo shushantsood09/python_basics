ee7b683c10e67e0d6036ebbb55c844c52e610909
shushant sood <104362678+shushantsood09@users.noreply.github.com>
2023-07-30 23:33:56 +0530

    calc program

---
+++ python_basics/calculators.py
@@ -0,0 +1,8 @@
+#Program to create a calculator
+
+a =56
+b = 3
+print("The value of ",a, "+" , b, "is", a+b)
+print("The value of ",a, "-" , b, "is", a-b)
+print("The value of ",a, "*" , b, "is", a*b)
+print("The value of ",a, "/" , b, "is", a/b)
--- python_basics/variables.py
+++ python_basics/variables.py
@@ -6,3 +6,10 @@ c= None
 print("The type of c is ",type(c))
 # print(a+b)   
 # Concatination in python should be done in same type of data types.
+
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
