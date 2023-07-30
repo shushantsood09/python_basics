a32773d80ad000ca761a4909a98231102749a15b
shushant sood <104362678+shushantsood09@users.noreply.github.com>
2023-07-30 23:13:50 +0530

    python variables

---
+++ python_basics/comments_and_esc_char.py
@@ -0,0 +1,7 @@
+print('Hey welcome to puthon \ndo conisitent to code.')
+# print("This doesnt "execute")
+print("This will \" execute")
+
+# print("hey", 6,7, sep="~", end="009/n")
+a =10
+print(type(a))
---
+++ python_basics/variables.py
@@ -0,0 +1,8 @@
+a = 5684568946587548
+print(a)
+b = "Shushant"
+print(b)
+c= None
+print("The type of c is ",type(c))
+# print(a+b)   
+# Concatination in python should be done in same type of data types.
