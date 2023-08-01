2f6bea774cfa420599555471c97475657bc26048
shushant sood <104362678+shushantsood09@users.noreply.github.com>
2023-08-01 23:52:55 +0530

    typecast in python

---
+++ python_basics/typecast.py
@@ -0,0 +1,19 @@
+# The conversion of one data type into another data type is called type casting or type conversion in Python
+
+a = "1"
+b = "2"
+print(a + b)  #Python do concat in case of string.
+
+a1 = 12
+b1 = 12
+print(a1 + b1)
+
+a3 = int("12")   # String should be a valid number to convert it in int data type.
+b3 = 6
+print(a3 + b3)
+
+#Implicit type casting
+
+c = 1.9
+d = 8 
+print(c +d)
