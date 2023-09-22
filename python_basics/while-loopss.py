7ebaa7473fb441a4d00590b989c5282537ee0cdf
shushant sood <104362678+shushantsood09@users.noreply.github.com>
2023-09-22 18:37:20 +0530

    while loop

---
+++ python_basics/while-loopss.py
@@ -0,0 +1,14 @@
+i = 0
+while (i <= 3):
+    print(i)
+    i += 1
+print("Done with the loop")
+
+# Decrementing while loop
+
+count = 5
+while (count > 0):
+    print(count)
+    count -= 1
+else:
+    print("Inside else")
