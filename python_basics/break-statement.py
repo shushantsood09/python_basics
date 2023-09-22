417c142e793d60d70b83d651a3771060e6ebce9a
shushant sood <104362678+shushantsood09@users.noreply.github.com>
2023-09-22 22:26:06 +0530

    functions.py

---
+++ python_basics/break-statement.py
@@ -0,0 +1,15 @@
+# Break statement
+for i in range(1,101,1):
+    print(i ,end=" ")
+    if(i==50):
+        break
+    else:
+        print("Mississippi")
+print("Thank you")
+
+
+# Continue Statement
+for i in [2,3,4,6,8,0]:
+    if (i%2!=0):
+        continue
+    print(i)
---
+++ python_basics/functions.py
@@ -0,0 +1,38 @@
+def calculateGMean(a, b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isGreater(a,b):
+    if(a > b):
+        print("First number is grater")
+    else:
+        print("Second number is grater or equal")
+
+def isLesser(a,b):
+    pass
+
+a = 9
+b = 8
+# gmean = (a*b)/(a+b)
+# print(gmean)
+
+# if(a > b):
+#     print("First number is grater")
+# else:
+#     print("Second number is grater or equal")
+isGreater(a,b)
+calculateGMean(a, b)
+
+
+c= 8
+d = 12
+# gmean = (a*b)/(a+b)
+# print(gmean)
+
+# if(c > d):
+#     print("First number is grater")
+# else:
+#     print("Second number is grater or equal")
+isGreater(c,d)
+
+calculateGMean(c, d)
