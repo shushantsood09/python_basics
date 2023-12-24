e74a6882e9dfb530c69de302e10271cfb765da3d
shushant sood <104362678+shushantsood09@users.noreply.github.com>
2023-12-24 23:15:38 +0530

    reduce in python.

---
+++ python_basics/reduce.py
@@ -0,0 +1,9 @@
+# We need to import reduce.
+
+from functools import reduce
+# List of numbers
+numbers = [1, 2, 3, 4, 5]
+# Calculate the sum of the numbers using the reduce function
+sum = reduce(lambda x, y: x + y, numbers)
+# Print the sum
+print(sum)
