cfd74ea92e5a33152a4f6a6832c60e07182a01fd
shushant sood <104362678+shushantsood09@users.noreply.github.com>
2024-01-27 18:57:24 +0530

    oops intro.

---
+++ python_basics/file-handling/Oop's/intro-to-oops.py
@@ -0,0 +1,13 @@
+sales1: 500
+profit1:200
+adspent: 100
+
+
+# RailWayForm -> Class [blueprint]
+# Shushant -> Shushant ki info vala form. -> object [entity]
+# Aman -> Aman ki info vala form. ->  object [entity]
+# Manoj -> Manoj ki info vala form. ->  object [entity]
+# Sam -> Sam ki info vala form. ->  object [entity]
+
+# If we want to change name or any info.
+# shushant.changeName("Shushant")
