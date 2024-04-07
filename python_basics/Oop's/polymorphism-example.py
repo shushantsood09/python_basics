e01c49f71a75b749038a5a532efc5dc12f76ba1d
shushant sood <104362678+shushantsood09@users.noreply.github.com>
2024-04-07 15:16:54 +0530

    polymorphism in python.

---
+++ python_basics/Oop's/polymorphism-example.py
@@ -0,0 +1,10 @@
+print(1 + 2)  # 3
+print("abc" + "def")  #concatenate -> abcdef
+print([1, 2, 3] + [4, 5, 6]) #merge
+
+
+class Student:
+    def __init__(self, phy, chem, math):
+        self.phy = phy
+        self.chem = chem
+        self.math = math
