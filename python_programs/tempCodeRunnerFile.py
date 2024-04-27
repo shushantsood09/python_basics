f407431d2be8fcfd3fc720c0eea0dc66baaf646c
shushant sood <104362678+shushantsood09@users.noreply.github.com>
2024-04-27 16:20:28 +0530

    Email verification program.

--- python_programs/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-
-k, j, d = 0, 0, 0
-if len(email) >= 6:  #1
-    if email[0].isalpha():   #2
-        if ("@" in email) and (email.count("@") == 1):    #3
-            if (email[-4] == ".") ^ (email[-3] == "."):    #4
